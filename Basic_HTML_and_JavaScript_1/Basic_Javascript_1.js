a63732a0b94bda1b0a910cf0c4814d57f42cc8a4
daniel-f-9 <danielmakesmoves9@gmail.com>
2026-02-25 21:27:30 -0800

    made chagnes
    made changes to html js doc

---
+++ Basic_HTML_and_JavaScript_1/Basic_Javascript_1.js
@@ -0,0 +1,8 @@
+alert("Hello, World! I Am An Alert Box!");
+
+function My_First_Function() {
+			var String = "Kisss me, I'm Irish!";
+			var result = String.fontcolor("green");
+			document.getElementById("Irish").innerHTML =
+			result;
+		};
